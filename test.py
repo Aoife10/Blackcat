ecd697d72aa8a5ee863ad390f1afbc6b86845b87
Aoife10 <a.o.sullivan@student.rug.nl>
2017-01-22 14:01:43 +0100

    Test python

---
+++ test.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+MyInt = 10
+MyFloat = 2.5
+MyString = "X:"
+
+
+
+MyOut = "%d,%5f" % (MyInt,MyFloat)
+
+Result = "%.1f" % (MyInt/MyFloat)
+
+print "Result:%fpercent" % (MyInt/MyFloat)
